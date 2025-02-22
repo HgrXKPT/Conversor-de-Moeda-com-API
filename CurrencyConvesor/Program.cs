88433f911c872b9415478fb981b75ab17e4cde1b
Higor <higorestevao02@hotmail.com>
2025-02-21 22:29:22 -0300

    Add project files.

---
+++ CurrencyConvesor/Program.cs
@@ -0,0 +1,48 @@
+﻿using Newtonsoft.Json;
+using static CurrencyConvesor.RespostaAPI;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Globalization;
+using CurrencyConvesor;
+
+class Program {
+    private static readonly HttpClient client = new HttpClient();
+
+    static async Task Main(string[] args) {
+        try {
+            Console.WriteLine("Escolha uma moeda para converter");
+            string fromCurrency = Console.ReadLine();
+            Console.WriteLine("Para qual moeda deseja converter?");
+            string toCurrency = Console.ReadLine();
+            string url = $"https://economia.awesomeapi.com.br/json/last/{fromCurrency}-{toCurrency}";
+
+            var response = await client.GetStringAsync(url);
+            var exchangeRateResponse = JsonConvert.DeserializeObject<Dictionary<string,ExchangeRateResponse>>(response);
+            string key = $"{fromCurrency}{toCurrency}";
+
+            Console.WriteLine($"Escolha um valor em {fromCurrency} Para ser convertido. Obs: por em numero real");
+            decimal valor = decimal.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+            Console.WriteLine(valor);
+
+
+
+
+
+            if(exchangeRateResponse.TryGetValue($"{fromCurrency + toCurrency}",out ExchangeRateResponse rate)) {
+                Console.WriteLine($"Taxa de câmbio de {fromCurrency} para {toCurrency}: {rate.Bid}");
+                decimal result = valor * rate.Bid;
+
+                Console.WriteLine($"Convertendo o valor que foi colocado ficaria: {result:F2}");
+            } else {
+                Console.WriteLine($"Não foi possível encontrar a taxa de câmbio para {fromCurrency}-{toCurrency}");
+            }
+        } catch(HttpRequestException ex) {
+            Console.WriteLine($"Alguma das moedas não existem, Código de error: {ex}");
+        } catch(Exception ex) {
+            Console.WriteLine($"Algum error inusitado aconteceu {ex}");
+        }
+
+        
+    }
+}
---
+++ CurrencyConvesor/RespostaAPI.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CurrencyConvesor {
+    internal class RespostaAPI {
+
+        public class ExchangeRateResponse {
+            public string Code { get; set; }
+            public string Codein { get; set; }
+            public string Name { get; set; }
+            public decimal High { get; set; }
+            public decimal Low { get; set; }
+            public decimal VarBid { get; set; }
+            public decimal PctChange { get; set; }
+            public decimal Bid { get; set; }
+            public decimal Ask { get; set; }
+            public long Timestamp { get; set; }
+            public string CreateDate { get; set; }
+        }
+
+    }
+}
